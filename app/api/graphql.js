d69b45d2b97585c9ea1b880e380fc70ac0888224
ValentaCodes <valentacodes@gmail.com>
2023-06-18 17:23:08 -0600

    Initial GraphQL files

---
+++ app/api/graphql.js
@@ -0,0 +1,19 @@
+import { ApolloServer } from '@apollo/server';
+import { startServerAndCreateNextHandler } from '@as-integrations/next';
+import { ApolloServerPluginLandingPageGraphQLPlayground } from 'apollo-server-core';
+
+import typeDefs from '@/graphql/schemas';
+import resolvers from '@/graphql/resolvers';
+import allowCors from '@/utils/cors';
+
+const apolloServer = new ApolloServer({
+  typeDefs,
+  resolvers,
+
+});
+
+const handler = startServerAndCreateNextHandler(apolloServer, {
+  context: async (req, res) => ({ req, res }),
+});
+
+export default allowCors(handler);
